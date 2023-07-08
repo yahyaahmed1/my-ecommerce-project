c136f50f4358ccb23cac720e824b6e1743662a2c
Yahyaahmed1 <yahyaahmedfarouk6@gmail.com>
2023-07-08 13:09:02 +0300

    checkout-page-done

--- src/index.js
+++ src/index.js
@@ -17,30 +17,6 @@ document.querySelectorAll(".add-cart-btn").forEach(el => {
   });
 });
 
-// تغير تنسيق المقاسات والاحجام
-let sizeOption = document.querySelectorAll(".size-option");
-let sizeRadios = document.querySelectorAll(".size-option input[type='radio']");
-sizeRadios.forEach(item => {
-  item.addEventListener("click", () => {
-    sizeOption.forEach(i => {
-      i.classList.remove("active");
-    })
-    item.parentNode.parentNode.classList.add("active")
-  })
-})
-
-let colorOption = document.querySelectorAll(".color-option");
-let colorRadios = document.querySelectorAll(".color-option input[type='radio']");
-colorRadios.forEach(item => {
-  item.addEventListener("click", () => {
-    colorOption.forEach(i => {
-      i.classList.remove("active");
-    })
-    item.parentNode.parentNode.classList.add("active")
-  })
-})
-
-
 // التحقق من صحة البريد الالكترونى
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
@@ -64,3 +40,61 @@ colorRadios.forEach(item => {
 
 // لاضافة السنة في الحقوق محفوظة
 document.getElementById("fullYear").innerHTML = new Date().getFullYear();
+
+// حساب اجمالي عربة الشراء
+function calculateTotalOrder() {
+  let totalPriceForAll = 0
+  document.querySelectorAll('[data-product-info]').forEach(product => {
+    const quantity = product.querySelector('[data-product-quantity]').value;
+    const price = product.getAttribute('data-product-price');
+    const total = quantity * price;
+    totalPriceForAll += total;
+  })
+  document.getElementById('total-productions-price').innerHTML = totalPriceForAll + "$"
+}
+
+// حساب السعر الاجمالي للمنتجات
+document.querySelectorAll('[data-product-quantity]').forEach(item => {
+  item.addEventListener('change', () => {
+    const newQuantity = item.value;
+    const parent = item.closest('[data-product-info]');
+    const price = parent.getAttribute('data-product-price');
+    const totalPriceForProduct = newQuantity * price;
+    parent.querySelector(".total-price-for-product").innerHTML = totalPriceForProduct + "$";
+
+    calculateTotalOrder()
+
+  })
+})
+
+// زر حذف المنتجات
+document.querySelectorAll('[data-remove-btn]').forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.closest('[data-product-info]').remove()
+    // تحديث حساب اجمالي عربة الشراء
+    calculateTotalOrder()
+  })
+})
+
+// تغير تنسيق المقاسات والاحجام
+let sizeOption = document.querySelectorAll(".size-option");
+let sizeRadios = document.querySelectorAll(".size-option input[type='radio']");
+sizeRadios.forEach(item => {
+  item.addEventListener("click", () => {
+    sizeOption.forEach(i => {
+      i.classList.remove("active");
+    })
+    item.parentNode.parentNode.classList.add("active")
+  })
+})
+
+let colorOption = document.querySelectorAll(".color-option");
+let colorRadios = document.querySelectorAll(".color-option input[type='radio']");
+colorRadios.forEach(item => {
+  item.addEventListener("click", () => {
+    colorOption.forEach(i => {
+      i.classList.remove("active");
+    })
+    item.parentNode.parentNode.classList.add("active")
+  })
+})
